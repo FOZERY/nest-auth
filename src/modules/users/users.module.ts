fd4ff8898dd3a2db52e4feb56c290f548f3951a3
fozery <tagievd31@gmail.com>
2025-02-28 10:00:20 +0300

    fix: PersonalProfileController import in users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,14 +2,14 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "../../external/persistence/prisma/prisma.module";
 import { TokenModule } from "../token/token.module";
 import { UsersRepositoryImpl } from "./external/prisma/users.repository.impl";
-import { ProfileController } from "./profile.controller";
+import { PersonalProfileController } from "./profile.controller";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
 	imports: [PrismaModule, TokenModule],
 	providers: [UsersRepositoryImpl, UsersService],
-	controllers: [UsersController, ProfileController],
+	controllers: [UsersController, PersonalProfileController],
 	exports: [UsersService],
 })
 export class UsersModule {}
