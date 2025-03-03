efc1ae37fc3a560852e84decb45732daf5617132
fozery <tagievd31@gmail.com>
2025-03-03 12:20:57 +0300

    fix: fix swagger for RefreshTokenRequestDTO

--- src/modules/auth/dto/requests/refresh-token.request.dto.ts
+++ src/modules/auth/dto/requests/refresh-token.request.dto.ts
@@ -1,17 +1,35 @@
-import { IsIP, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsIP, IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
 
 export class RefreshTokenRequestDTO {
+	@ApiProperty({
+		description: "Fingerprint",
+		example: "123e4567-e89b-12d3-a456-426614174000",
+	})
+	@IsNotEmpty()
+	@IsString()
+	@MaxLength(255)
+	fingerprint: string;
+
+	@ApiProperty({
+		description: "Ip address",
+		example: "192.168.0.1",
+		required: false,
+	})
 	@IsOptional()
 	@IsNotEmpty()
 	@IsIP()
 	ipAddress?: string;
 
-	@IsNotEmpty()
-	@IsString()
-	fingerprint: string;
-
+	@ApiProperty({
+		description: "User agent",
+		example:
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+		required: false,
+	})
 	@IsOptional()
 	@IsNotEmpty()
 	@IsString()
+	@MaxLength(255)
 	userAgent?: string;
 }
