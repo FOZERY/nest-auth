df5bdd8a2db79d7ca5a8c2b6798a0eaf7bfe356b
fozery <tagievd31@gmail.com>
2025-03-31 12:37:02 +0300

    refactor(swagger): remove unused fingerprint header documentation
    - Delete fingerprint header Swagger documentation as it is no longer needed for the API specification

--- src/external/swagger/fingerprintHeader.swagger.ts
+++
@@ -1,9 +0,0 @@
-export const fingerprintSwaggerHeader = {
-	fingerprint: {
-		description: "Уникальный идентификатор устройства",
-		schema: {
-			type: "string",
-			example: "6fd4s86f1ds68f41ds8f4ds",
-		},
-	},
-};
