b00411a0c666d72d374bb266459d67158941ae80
fozery <tagievd31@gmail.com>
2025-04-21 15:28:05 +0300

    refactor(infrastructure): update NATS and Prisma configuration
    - Update NATS module configuration\n- Update Prisma service configuration\n- Add Prisma adapter type\n- Update CLS transactional configuration

--- apps/users-service/src/external/nats/nats.service.ts
+++ apps/users-service/src/external/nats/nats.service.ts
@@ -1,36 +1,85 @@
-import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { NatsJetStreamClientProxy } from "@nestjs-plugins/nestjs-nats-jetstream-transport";
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { NatsConnection } from "nats";
+import pRetry from "p-retry";
+import { firstValueFrom, Subscription } from "rxjs";
+
+interface PublishConfig {
+	maxRetries: number;
+	baseDelay: number;
+}
 
 @Injectable()
-export class NatsService implements OnModuleInit {
+export class NatsService implements OnModuleInit, OnModuleDestroy {
 	private readonly logger = new Logger(NatsService.name);
+	private readonly defaultConfig: PublishConfig = {
+		maxRetries: 1,
+		baseDelay: 1000,
+	};
+	private statusSubscription: Subscription;
+	private nc: NatsConnection;
 
-	private client: ClientProxy;
-
-	constructor(private readonly configService: ConfigService) {
-		this.client = ClientProxyFactory.create({
-			transport: Transport.NATS,
-			options: {
-				servers: this.configService.get<string>("NATS_URL")!,
-				user: this.configService.get<string>("NATS_USER")!,
-				password: this.configService.get<string>("NATS_PASSWORD")!,
-			},
-		});
-	}
+	constructor(private readonly client: NatsJetStreamClientProxy) {}
 
 	async onModuleInit() {
-		await this.client.connect();
-		this.client.status.subscribe((status) => {
-			this.logger.debug(`Nats status: ${status}`);
-		});
+		this.nc = await this.client.connect();
+		this.listenToStatus();
+	}
+
+	private async listenToStatus() {
+		try {
+			for await (const status of this.nc.status()) {
+				this.logger.debug({ status }, "NATS status update");
+			}
+		} catch (error) {
+			this.logger.error({ error }, "Error listening to NATS status");
+		}
 	}
 
 	async onModuleDestroy() {
+		if (this.statusSubscription) {
+			this.statusSubscription.unsubscribe();
+		}
 		await this.client.close();
+		this.logger.debug({}, "NATS disconnected");
 	}
 
-	publish(topic: string, event: any): void {
-		this.client.emit(topic, event);
+	async publish(topic: string, event: any, config?: Partial<PublishConfig>): Promise<void> {
+		const { maxRetries, baseDelay } = { ...this.defaultConfig, ...config };
+
+		const operation = async (attempt: number) => {
+			this.logger.verbose(
+				`Publishing event to ${topic} (attempt ${attempt}/${maxRetries}): ${JSON.stringify(event)}`
+			);
+
+			try {
+				await firstValueFrom(this.client.emit(topic, event));
+				this.logger.verbose(`Successfully published event to ${topic}`);
+			} catch (error) {
+				this.logger.error(`Error publishing event to ${topic}: ${error}`);
+				throw error;
+			}
+		};
+
+		try {
+			await pRetry(operation, {
+				forever: maxRetries === -1,
+				retries: maxRetries - 1,
+				minTimeout: baseDelay,
+				factor: 2,
+				randomize: true,
+				onFailedAttempt: (error) => {
+					this.logger.warn(
+						`Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`
+					);
+				},
+			});
+		} catch (error) {
+			this.logger.error(
+				{ error },
+				`Failed to publish event to ${topic} after ${maxRetries} attempts`
+			);
+			throw error;
+		}
 	}
 }
--- apps/users-service/src/external/persistence/cls-transactional/cls.config.ts
+++ apps/users-service/src/external/persistence/cls-transactional/cls.config.ts
@@ -1,5 +1,6 @@
 import { ClsPluginTransactional } from "@nestjs-cls/transactional";
 import { TransactionalAdapterPrisma } from "@nestjs-cls/transactional-adapter-prisma";
+import { PrismaClient } from "@prisma/users-client";
 import { ClsModuleOptions } from "nestjs-cls";
 import { PrismaModule } from "../prisma/prisma.module";
 import { PrismaService } from "../prisma/prisma.service";
@@ -8,7 +9,7 @@ export const clsConfig: ClsModuleOptions = {
 	plugins: [
 		new ClsPluginTransactional({
 			imports: [PrismaModule],
-			adapter: new TransactionalAdapterPrisma({
+			adapter: new TransactionalAdapterPrisma<PrismaClient>({
 				prismaInjectionToken: PrismaService,
 			}),
 		}),
