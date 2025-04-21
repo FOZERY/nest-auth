1f93e9dbd5c5c372f4354b24b49a8ff73620bc6c
fozery <tagievd31@gmail.com>
2025-04-21 15:29:14 +0300

    chore: remove old Prisma migrations and schema
    - Remove all old migrations\n- Remove schema.prisma\n- Remove migration_lock.toml

--- prisma/migrations/20250225123248_add_default_to_status_for_refresh_sessions/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "refresh_sessions" ALTER COLUMN "status" SET DEFAULT 'active';
--- prisma/migrations/20250226093340_fix_remove_users_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `usersId` on the `refresh_sessions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "refresh_sessions" DROP COLUMN "usersId";
